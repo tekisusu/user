1b706468a0a3a54ae11b606f9b23ef420a0b65ce
tekisusu <tekisusu@gmail.com>
2024-08-20 18:35:22 -0500

    Modified: assets/excelTemplate/datos.xlsl
    Added: Weeks Section

--- pages/script.js
+++ pages/script.js
@@ -36,7 +36,6 @@ export function mostrarDatos() {
       row.innerHTML = `
         <td class="text-center">${filaNumero++}</td>
         <td class="text-center">${user.nombre}</td>
-        <td class="text-center"><span class="${!user.semana ? 'invisible-value' : ''}" data-field="semana">${user.semana || ''}</span></td>
         <td class="text-center attendance-cell"></td>
         <td class="text-center"><span class="${!user.lunes ? 'invisible-value' : ''}" data-field="lunes">${user.lunes || ''}</span></td>
         <td class="text-center"><span class="${!user.martes ? 'invisible-value' : ''}" data-field="martes">${user.martes || ''}</span></td>
@@ -56,7 +55,7 @@ export function mostrarDatos() {
 
     spans.forEach((span) => {
       const textContent = span.textContent.trim();
-      if (textContent === "12.00" || textContent === "Completado") {
+      if (textContent === "12.00" || textContent === "Completo") {
         span.style.color = "green";
         span.style.fontWeight = "bold";
       }
